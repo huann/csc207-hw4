25a5e79b2bdbe6efc2f349fa7deab56f7ce4c0e5
Kyle <moorehea1@grinnell.edu>
2013-09-24 21:32:31 -0500

    User interface functional, helper for storage still not working correctly.

--- src/edu/grinnell/csc207/huann/hw4/Calculator.java
+++ src/edu/grinnell/csc207/huann/hw4/Calculator.java
@@ -3,9 +3,33 @@ package edu.grinnell.csc207.huann.hw4;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 
+/**
+ * A simple implementation of fractions.
+ * 
+ * @author Samuel A. Rebelsky
+ * @author Ann Hu
+ * @author Kyle Moorehead
+ * @author Tim Youtz
+ * @author CSC207 2013F
+ * @version 0.1 of September 2013
+ */
+
 public class Calculator {
+	
+	    // +------------------+---------------------------------------------
+		// | Design Decisions |
+		// +------------------+
+
+		/*
+		 * User input must be separated by spaces.
+		 * 
+		 * The calculator returns only fractions, in their simplest form.
+		 * 
+		 */
+
 
 	// static String[] r = new String[8];
+	static Fraction[] r = new Fraction[8];
 
 	public static Fraction evaluate(String expression) throws Exception {
 		// use the spaces in the expression as a separator
@@ -13,34 +37,38 @@ public class Calculator {
 		// take the first number as temporary answer
 		Fraction ans = new Fraction(BigInteger.valueOf(1),
 				BigInteger.valueOf(1));
-		Fraction[] r = new Fraction[8];
+		
+		
+		if (arrExpr.length == 1) {
+			ans = new Fraction(rCheck(arrExpr[0]));
+			return ans; //if there is only one input, return that input (assuming it can be evaluated)
+		} else {
+			if (arrExpr[1].equals("=")) {
+				r[(int) arrExpr[0].charAt(1)] = evaluate(rCheck(arrExpr[2]));
+				return r[(int) arrExpr[0].charAt(1)];
+				//if the operator is "=" then set the first expression (r[something]) equal to the second expression
+			}
+			ans = new Fraction(rCheck(arrExpr[0]));
+			// set the current answer to the first number input
+			for (int i = 1; i < arrExpr.length; i++) {
 
-		if (arrExpr[1].equals("=")) {
-			r[(int) arrExpr[0].charAt(1)] = evaluate(arrExpr[2]);
-			return r[(int) arrExpr[0].charAt(1)] ;
-		}
-		ans = new Fraction(arrExpr[0]);
+				if (arrExpr[i].equals("+")) {
+					ans = ans.add(new Fraction(rCheck(arrExpr[i + 1])));
+				} // if addition
+				if (arrExpr[i].equals("-")) {
+					ans = ans.subtract(new Fraction(rCheck(arrExpr[i + 1])));
+				} // if subtraction
+				if (arrExpr[i].equals("*")) {
+					ans = ans.multiply(new Fraction(rCheck(arrExpr[i + 1])));
+				} // if multiplication
+				if (arrExpr[i].equals("/")) {
+					ans = ans.divide(new Fraction(rCheck(arrExpr[i + 1])));
+				} // if division
 
-		for (int i = 1; i < arrExpr.length; i++) {
-			//if (arrExpr[i - 1].charAt(0) == 'r') {
-			//	arrExpr[i - 1] = r[arrExpr[i - 1].charAt(1)];
-			//} // if
-			if (arrExpr[i].equals("+")) {
-				ans = ans.add(new Fraction(arrExpr[i + 1]));
-			} // if addition
-			if (arrExpr[i].equals("-")) {
-				ans = ans.subtract(new Fraction(arrExpr[i + 1]));
-			} // if subtraction
-			if (arrExpr[i].equals("*")) {
-				ans = ans.multiply(new Fraction(arrExpr[i + 1]));
-			} // if multiplication
-			if (arrExpr[i].equals("/")) {
-				ans = ans.divide(new Fraction(arrExpr[i + 1]));
-			} // if division
-			
-		} // for
-		return ans;
-	} // eva1uate(string)
+			} // for
+			return ans;
+		} // eva1uate(string)
+	}
 
 	public static Fraction[] evaluate(String[] expression) throws Exception {
 		Fraction[] ans = new Fraction[expression.length];
@@ -50,17 +78,33 @@ public class Calculator {
 		return ans;
 	} // eva1uate(string [])
 
+	public static String rCheck(String arrExpr) throws Exception {
+
+		if (arrExpr.charAt(0) == 'r') {
+			return (r[arrExpr.charAt(1)]).toString();
+		} else
+			return arrExpr;
+
+	}
+
 	public static void main(String[] args) throws Exception {
 		PrintWriter pen = new PrintWriter(System.out, true);
 		java.io.BufferedReader eyes;
 		java.io.InputStreamReader istream;
 		istream = new java.io.InputStreamReader(System.in);
-		eyes = new java.io.BufferedReader(istream);
 		String response;
+		eyes = new java.io.BufferedReader(istream);
 		pen.println("What would you like calculated?\nType 'quit' to exit calculator.");
-		while ((response = eyes.readLine()) != "quit") {
-			pen.println(Calculator.evaluate(response));
-			pen.println("Calculation complete.");
+		while (true) {
+			while ((response = eyes.readLine()).equals("quit")) {
+				System.exit(0);
+			}
+			try {
+				pen.println(Calculator.evaluate(rCheck(response)));
+				pen.println("Calculation complete.");
+			} catch (Exception e) {
+				pen.println("Sorry, that is not valid input. Please try again.");
+			}
 		}
 	}
 
